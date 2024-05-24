bf55b85c7318b8960150129a803233b55c068e54
valentinaotero <162721713+valentinaotero@users.noreply.github.com>
2024-05-24 12:35:18 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,20 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
-
-
-def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
-
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
-
+def get_coordinate(tuple):
+    coordinate = tuple [-1]
+    return str (coordinate)
 
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
-
+    numero= coordinate [0]
+    letra= coordinate [1]
+    return (numero,letra)
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
-
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-
-    return ()
+    
+    coordinate_rui= rui_record[1]
+    coordinate_azara= azara_record [1]
+    numero=coordinate_azara [0]
+    letra=coordinate_azara[1]
+    variable= (numero,letra)
+    if coordinate_rui==variable:
+        return azara_record + rui_record
+    else:
+        return "not a match"
